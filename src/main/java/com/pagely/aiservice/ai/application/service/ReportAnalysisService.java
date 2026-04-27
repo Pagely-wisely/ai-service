91c797c4b75f30985b4ed1d80308063871e9996c
gnoesnooj <306yyy@naver.com>
2026-04-27 12:50:52 +0900

    refactor: 독후감 분석 완료 후 도서 프로필 데이터 업데이트 로직 이벤트 방식으로 리팩토링

--- src/main/java/com/pagely/aiservice/ai/application/service/ReportAnalysisService.java
+++ src/main/java/com/pagely/aiservice/ai/application/service/ReportAnalysisService.java
@@ -1,9 +1,9 @@
 package com.pagely.aiservice.ai.application.service;
 
+import com.pagely.aiservice.ai.application.dto.ReportAnalysisCompletedEvent;
 import com.pagely.aiservice.ai.application.dto.ReportContentAnalysisResult;
 import com.pagely.aiservice.ai.application.dto.command.ReportCreatedCommand;
-import com.pagely.aiservice.ai.application.port.out.BookEmbeddingPort;
-import com.pagely.aiservice.ai.application.port.out.BookProfileGeneratorPort;
+import com.pagely.aiservice.ai.application.port.out.DomainEventPublisher;
 import com.pagely.aiservice.ai.application.port.out.ReportAnalysisPort;
 import com.pagely.aiservice.ai.domain.model.ReportAnalysis;
 import com.pagely.aiservice.ai.domain.repository.ReportAnalysisRepository;
@@ -16,10 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReportAnalysisService {
 
     private final ReportAnalysisRepository reportAnalysisRepository;
-
     private final ReportAnalysisPort reportAnalysisPort;
-    private final BookProfileGeneratorPort bookProfileGeneratorPort;
-    private final BookEmbeddingPort bookEmbeddingPort;
+    private final DomainEventPublisher eventPublisher;
 
     @Transactional
     public void handleReportCreated(ReportCreatedCommand command) {
@@ -36,13 +34,12 @@ public class ReportAnalysisService {
 
         reportAnalysisRepository.save(reportAnalysis);
 
-        // TODO : publish event - 도서에 대해 새롭게 생성된 독후감을 바탕으로 기존 도서의 프로필 텍스트 및 벡터값을 변경한다
-        String updatedProfileText = bookProfileGeneratorPort.generate(command.bookId(),
+        eventPublisher.publish(new ReportAnalysisCompletedEvent(
+                command.bookId(),
                 command.title(),
                 command.author(),
                 command.category(),
-                command.description(),
-                reportAnalysisRepository.findByBookId(command.bookId()));
-        bookEmbeddingPort.update(command.bookId(), updatedProfileText);
+                command.description()
+        ));
     }
 }
